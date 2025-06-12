128989cf6f2d319b9e67948f43553634e5d6ad75
Nguyễn Anh Tuấn <nguyenanhtuan39x@gmail.com>
2025-06-12 20:29:07 +0700

    Update capcut-premium.js

--- capcut-premium.js
+++ capcut-premium.js
@@ -1,1 +1,21 @@
 
+/**
+ * CapCut Premium Unlock Script
+ * Unlock by Shadowrocket Rewrite + Script
+ * Author: @yourusername
+ */
+
+let body = JSON.parse($response.body);
+
+body.data = {
+  "valid": true,
+  "product_id": "com.lemon.love.premium.yearly",
+  "expired_at": 4070880000,
+  "is_trial": false,
+  "is_activated": true,
+  "sub_type": "premium",
+  "platform": "ios",
+  "auto_renew_status": true
+};
+
+$done({ body: JSON.stringify(body) });
